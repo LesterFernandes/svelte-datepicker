e60ae76b0644fec9d0a763e578cfc1cbd4cd5fd3
L Fernandes <lestr.169@gmail.com>
2023-11-19 18:13:23 +1000

    project init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import '../app.css';
+	import { setContext } from 'svelte';
+
+	setContext('answer', 42);
+</script>
+
+<div class="container mx-auto p-20">
+    <h3 class="text-3xl underline">Datepicker</h3>
+</div>
+
+<style lang="postcss">
+</style>
